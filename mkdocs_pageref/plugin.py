4c3b66f41fe763506e5f9e5ad5eb531594bc8107
Douglas Sebastian <douglas.sebastian.mme@gmail.com>
2024-01-26 18:50:42 -0300

    v1.2.1 - fix references on build

--- mkdocs_pageref/plugin.py
+++ mkdocs_pageref/plugin.py
@@ -1,10 +1,13 @@
 import re
+import logging
 from mkdocs.plugins import BasePlugin
 from mkdocs.config.defaults import MkDocsConfig
 from mkdocs.structure.files import Files
 from mkdocs.structure.pages import Page
 from .replace import replace_matches
 from .pageref_config import PageRefConfig, PageReference
+
+logger = logging.getLogger(f"mkdocs.plugin.{__name__}")
 
 def get_pageref_pattern(page: Page) -> str | None:
 	value = page.meta.get("pageref-pattern", None)
@@ -26,23 +29,18 @@ class PageRefPlugin(BasePlugin[PageRefConfig]):
 	references: list[PageReference]
 
 	def on_config(self, config: MkDocsConfig) -> MkDocsConfig | None:
-		self.references = []
-
-	def on_files(self, files: Files, *, config: MkDocsConfig) -> Files | None:
-		self.references.extend([
+		logger.debug("Add global references")
+		self.references = [
 			PageReference(re.compile(m.pattern), m.page)
 			for m in self.config.matches
-		])
+		]
 
 	def on_page_markdown(self, markdown: str, *, page: Page, **_) -> str | None:
 		pattern = get_pageref_pattern(page)
-		page_path = page.abs_url
-
-		if page_path is None:
-			return
 
 		if pattern is not None:
-			self.references.append( PageReference(re.compile(pattern), page_path) )
+			logger.debug(f"Adding page reference for {page.file.name}")
+			self.references.append( PageReference(re.compile(pattern), page.url) )
 
 		subpatterns = get_pageref_subpatterns(page)
 		for element_id, pattern in subpatterns.items():
@@ -52,8 +50,10 @@ class PageRefPlugin(BasePlugin[PageRefConfig]):
 			if not element_id.startswith("#"):
 				element_id = "#" + element_id
 
-			self.references.append(PageReference(re.compile(pattern), page_path, element_id))
+			logger.debug(f"Adding page sub-reference for {element_id}")
+			self.references.append(PageReference(re.compile(pattern), page.url, element_id))
 
 	def on_post_page(self, output: str, *, page: Page, config: MkDocsConfig) -> str | None:
+		logger.debug(f"Applying references to {page.file.src_uri}")
 		references = [ref for ref in self.references if ref.destination != page.abs_url]
 		return replace_matches(output, references, self.config.reference_class)
